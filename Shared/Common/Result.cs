da653d3a1c017d7e3f91a23f25ae206feac74111
javierskiv2 <e.holmes.0517@gmail.com>
2024-05-04 18:50:27 -0500

    Clases comunes para DTOs de salida o respuestas del API

---
+++ Shared/Common/Result.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.Common
+{
+    public enum Result
+    {
+        [Display(Name = "Success")]
+        Success,
+
+        [Display(Name = "Error")]
+        Error,
+
+        [Display(Name = "NoRecords")]
+        NoRecords
+    }
+}
