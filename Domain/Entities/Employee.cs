c4f7e6aa9e56b52754adb696ef36f394284e148e
javierskiv2 <e.holmes.0517@gmail.com>
2024-05-04 23:41:23 -0500

    Ajuste de validaciones e implementacion de DataAnnotation personalizados

--- Domain/Entities/Employee.cs
+++ Domain/Entities/Employee.cs
@@ -1,34 +1,43 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Shared.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 
-namespace Domain.Entity
+namespace Domain.Entities
 {
     public class Employee
     {
-        [Required]
-        public int CompanyId { get; set; }
+        [NotZero(ErrorMessage = "El CompanyId es obligatorio.")]
+        [Required(ErrorMessage = "El CompanyId es obligatorio.")]
+        public int? CompanyId { get; set; }
         public DateTime CreatedOn { get; set; }
-        public DateTime DeletedOn { get; set; }
+        public DateTime? DeletedOn { get; set; }
 
-        [Required]
+        [EmailAddress(ErrorMessage = "El correo electrónico no tiene un formato válido.")]
+        [Required(ErrorMessage = "El correo electronico es obligatorio.")]
         public string Email { get; set; }
         public int EmployeeId { get; set; }
         public string Fax { get; set; }
         public string Name { get; set; }
         public DateTime LastLogin { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "El password es obligatorio.")]
         public string Password { get; set; }
 
-        [Required]
-        public int PortalId { get; set; }
-        [Required]
-        public int RoleId { get; set; }
-        [Required]
-        public int StatusId { get; set; }
+        [NotZero(ErrorMessage = "El PortalId es obligatorio.")]
+        [Required(ErrorMessage = "El PortalId es obligatorio.")]
+        public int? PortalId { get; set; }
+
+        [NotZero(ErrorMessage = "El RoleId es obligatorio.")]
+        [Required(ErrorMessage = "El RoleId es obligatorio.")]
+        public int? RoleId { get; set; }
+
+        [NotZero(ErrorMessage = "El StatusId es obligatorio.")]
+        [Required(ErrorMessage = "El StatusId es obligatorio.")]
+        public int? StatusId { get; set; }
         public string Telephone { get; set; }
-        public DateTime UpdateOn { get; set; }
-        
-        [Required]
+        public DateTime? UpdatedOn { get; set; }
+
+        [Required(ErrorMessage = "El Username es obligatorio.")]
         public string Username { get; set;}
+        public bool IsDeleted { get; set; } = false;
     }
 }
